423a21572cc059dcd162915022ee5ce4b3e42e92
Gordon Williams (u36) <gw@pur3.co.uk>
2016-07-23 14:25:30 +0100

    x axes

--- main.js
+++ main.js
@@ -138,10 +138,20 @@ function checkGamepad() {
     } else {
       if (v.y>threshMax) btns |= 1;
     }
+    if (btns&2) {
+      if (v.x<threshMin) btns &= ~2;
+    } else {
+      if (v.x>threshMax) btns |= 2;
+    }
     if (btns&4) {
       if (v.y>-threshMin) btns &= ~4;
     } else {
-      if (v.y<threshMax) btns |= 4;
+      if (v.y<-threshMax) btns |= 4;
+    }
+    if (btns&8) {
+      if (v.x>-threshMin) btns &= ~8;
+    } else {
+      if (v.x<-threshMax) btns |= 8;
     }
   }
   var newBtn = btns & ~lastGamepadBtns;
