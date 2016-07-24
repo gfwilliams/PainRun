ecca55d8c3869cbee8fe25e617c66e656f05c270
Gordon Williams (u36) <gw@pur3.co.uk>
2016-07-24 13:42:54 +0100

    less harsh stereo

--- title.js
+++ title.js
@@ -8,21 +8,21 @@ svgDoc.getElementById("fullscreen").addEventListener('click', function() {
 });
 svgDoc.getElementById("cardboard_gyro").addEventListener('click', function() {
   STEREO = true;
-  STEREO_SEP = 0.4;
+  STEREO_SEP = 0.3;
   USE_GYRO = true;
   svg.remove();
   init();
 });
 svgDoc.getElementById("cardboard_nogyro").addEventListener('click', function() {
   STEREO = true;
-  STEREO_SEP = 0.4;
+  STEREO_SEP = 0.3;
   USE_GYRO = false;
   svg.remove();
   init();
 });
 svgDoc.getElementById("crosseyed").addEventListener('click', function() {
   STEREO = true;
-  STEREO_SEP = -0.2;
+  STEREO_SEP = -0.1;
   USE_GYRO = false;
   svg.remove();
   init();
